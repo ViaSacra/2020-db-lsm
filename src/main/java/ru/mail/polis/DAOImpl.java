d40ad932e94df2f00b4f8dd93ed60ddc6d279444
v.ivlev <v.ivlev@geoscan.aero>
2020-04-20 21:32:43 +0300

    Implementation DAO
    First feature (In-memory)

---
+++ src/main/java/ru/mail/polis/DAOImpl.java
@@ -0,0 +1,36 @@
+package ru.mail.polis;
+
+import com.google.common.collect.Iterators;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class DAOImpl implements DAO {
+    private final SortedMap<ByteBuffer, ByteBuffer> map = new TreeMap<>();
+
+    @NotNull
+    @Override
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) {
+        return Iterators.transform(
+                map.tailMap(from).entrySet().iterator(),
+                e -> Record.of(Objects.requireNonNull(e).getKey(), e.getValue()));
+    }
+
+    @Override
+    public void upsert(@NotNull final ByteBuffer key, @NotNull ByteBuffer value) {
+        map.put(key, value);
+    }
+
+    @Override
+    public void remove(@NotNull final ByteBuffer key) {
+        map.remove(key);
+    }
+
+    @Override
+    public void close() {
+    }
+}
